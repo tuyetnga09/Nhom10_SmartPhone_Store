c742ee261753f251bfc252237492cd4335838859
tuyetnga09 <ngapttph23404@fpt.edu.vn>
2023-06-07 23:49:48 +0700

    add

---
+++ src/main/java/com/example/smartphone_store/SmartPhoneStoreApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartphone_store;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartPhoneStoreApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SmartPhoneStoreApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/smartphone_store/SmartPhoneStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.smartphone_store;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmartPhoneStoreApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
