ec88f8ffac65f5d3109581dd2cad0c8aca704bc4
hieuhoang03 <hieuhv203@gmail.com>
2023-06-08 22:46:09 +0700

    Rename fiel Application

--- src/main/java/com/example/smartphone_store/Application.java
+++ src/main/java/com/example/smartphone_store/Application.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SmartPhoneStoreApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SmartPhoneStoreApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
