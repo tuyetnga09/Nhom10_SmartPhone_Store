d1df28efa29777ccd529c557f97c5188880ebf43
hieuhoang03 <hieuhv203@gmail.com>
2023-06-11 22:23:46 +0700

    Create class ImagesService

---
+++ src/main/java/com/example/smartphone_store/service/impl/ImagesService.java
@@ -0,0 +1,41 @@
+package com.example.smartphone_store.service.impl;
+
+import com.example.smartphone_store.entity.Images;
+import com.example.smartphone_store.repository.ImagesRepository;
+import com.example.smartphone_store.service.IImages;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class ImagesService implements IImages {
+
+    @Autowired
+    private ImagesRepository repository;
+
+    @Override
+    public List<Images> findAll(int status) {
+        return this.repository.selectByStatus(status);
+    }
+
+    @Override
+    public void save(Images images) {
+        this.repository.save(images);
+    }
+
+    @Override
+    public void update(Images images) {
+        this.repository.save(images);
+    }
+
+    @Override
+    public void delete(Long id) {
+        this.repository.delete(id);
+    }
+
+    @Override
+    public Images findById(Long id) {
+        return this.repository.findById(id).orElse(null);
+    }
+}
