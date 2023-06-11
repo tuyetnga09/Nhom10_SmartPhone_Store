6a658dccb4fe4f07891628b5d32c0151ef7156d1
hieuhoang03 <hieuhv203@gmail.com>
2023-06-11 20:45:00 +0700

    Create class ImagesDAO

---
+++ src/main/java/com/example/smartphone_store/DAO/ImagesDAO.java
@@ -0,0 +1,15 @@
+package com.example.smartphone_store.DAO;
+
+import org.springframework.web.multipart.MultipartFile;
+
+public class ImagesDAO {
+
+    private MultipartFile fileImages;
+
+    private String describe;
+
+    private String personCreate;
+
+    private String personUpdate;
+
+}
