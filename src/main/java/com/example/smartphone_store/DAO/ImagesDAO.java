1f508ffd58d2c6a9bd7339a7d27c45547673c349
hieuhoang03 <hieuhv203@gmail.com>
2023-06-11 21:55:44 +0700

    Import attribute in class ImagesDAO

--- src/main/java/com/example/smartphone_store/DAO/ImagesDAO.java
+++ src/main/java/com/example/smartphone_store/DAO/ImagesDAO.java
@@ -1,13 +1,17 @@
 package com.example.smartphone_store.DAO;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import org.springframework.web.multipart.MultipartFile;
 
 public class ImagesDAO {
 
+    @NotNull(message = "Chưa chọn ảnh!")
     private MultipartFile fileImages;
 
     private String describe;
 
+    @NotBlank(message = "Người nhập trống!")
     private String personCreate;
 
     private String personUpdate;
