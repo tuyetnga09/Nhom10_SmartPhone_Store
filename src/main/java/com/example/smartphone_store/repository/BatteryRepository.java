c2014232108cda53f30b0644f1ed33fbb25f2764
ThuyLtd03 <thuyltdph23050@fpt.edu.vn>
2023-06-11 22:47:41 +0700

    create class BatteryRepository

---
+++ src/main/java/com/example/smartphone_store/repository/BatteryRepository.java
@@ -0,0 +1,21 @@
+package com.example.smartphone_store.repository;
+
+import com.example.smartphone_store.entity.Battery;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+public interface BatteryRepository extends JpaRepository<Battery, Long> {
+    @Transactional
+    @Modifying
+    @Query("update Battery set status = 0 where id = ?1")
+    void delete(int id);
+
+    @Query("select btr from Battery btr where btr.status = 1")
+    List<Battery> SelectWhereStatus();
+
+}
