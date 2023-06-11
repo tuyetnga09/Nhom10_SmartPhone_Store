197e14bdd71929dc993544d5da6b60a01a5d2201
hieuhoang03 <hieuhv203@gmail.com>
2023-06-11 22:49:43 +0700

    Create form insert Images

--- src/main/java/com/example/smartphone_store/repository/ImagesRepository.java
+++ src/main/java/com/example/smartphone_store/repository/ImagesRepository.java
@@ -2,8 +2,10 @@ package com.example.smartphone_store.repository;
 
 import com.example.smartphone_store.entity.Images;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -13,6 +15,8 @@ public interface ImagesRepository extends JpaRepository<Images, Long> {
     @Query("select im from Images im where im.status=?1")
     List<Images> selectByStatus(int status);
 
+    @Transactional
+    @Modifying
     @Query("update Images set status = 0 where id = ?1")
     void delete(Long id);
 
