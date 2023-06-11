019b17c21ccfd84faafb8d8607821b927f44dd02
hieuhoang03 <hieuhv203@gmail.com>
2023-06-11 21:57:41 +0700

    Create interface ImagesRepository

---
+++ src/main/java/com/example/smartphone_store/repository/ImagesRepository.java
@@ -0,0 +1,8 @@
+package com.example.smartphone_store.repository;
+
+import com.example.smartphone_store.entity.Images;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ImagesRepository extends JpaRepository<Images, Long> {
+
+}
