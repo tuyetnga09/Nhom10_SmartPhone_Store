f36a651b32dd04e99d2c2c0e120226a216cf809b
hieuhoang03 <hieuhv203@gmail.com>
2023-06-16 22:56:08 +0700

    Update fille IProduct

--- src/main/java/com/example/smartphone_store/service/IProduct.java
+++ src/main/java/com/example/smartphone_store/service/IProduct.java
@@ -1,7 +1,19 @@
 package com.example.smartphone_store.service;
 
+import com.example.smartphone_store.entity.Product;
+
+import java.util.List;
+
 public interface IProduct {
 
+    List<Product> selectByStatus(Long status);
 
+    void save(Product product);
+
+    void update(Product product);
+
+    void delete(Long id);
+
+    Product findById(Long id);
 
 }
