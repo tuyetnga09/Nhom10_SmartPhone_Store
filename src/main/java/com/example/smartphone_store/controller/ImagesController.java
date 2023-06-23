e1cc38f916b5d8b81bf1b121f521d0585de9c8b7
hieuhoang03 <hieuhv203@gmail.com>
2023-06-23 10:13:02 +0700

    Pass Sql

--- src/main/java/com/example/smartphone_store/controller/ImagesController.java
+++ src/main/java/com/example/smartphone_store/controller/ImagesController.java
@@ -20,8 +20,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.multipart.MultipartFile;
 
+import org.springframework.web.multipart.MultipartFile;
 import java.io.IOException;
 import java.io.InputStream;
 import java.nio.file.Files;
