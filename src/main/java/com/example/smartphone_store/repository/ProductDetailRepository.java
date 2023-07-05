1267f833a9140c95f53ca2e7ef38707439c00b18
MinhTC23 <trankongmin@gmail.com>
2023-07-05 22:26:08 +0700

    update list_productdetail_bigger20000000.html

--- src/main/java/com/example/smartphone_store/repository/ProductDetailRepository.java
+++ src/main/java/com/example/smartphone_store/repository/ProductDetailRepository.java
@@ -41,6 +41,6 @@ public interface ProductDetailRepository extends JpaRepository<ProductDetail, Lo
 
     Page<ProductDetail> findProductDetailByStatusAndProductId(Integer status, Long id, Pageable pageable);
 
-    @Query(value = "select ProductDetail.* from ProductDetail join Product on ProductDetail.Id_Product = Product.Id where ProductDetail.Status = 0 and Product.Price <= 20000000", nativeQuery = true)
+    @Query(value = "select ProductDetail.Id, ProductDetail.Code, ProductDetail.Name, ProductDetail.DateCreate, ProductDetail.DateUpdate, ProductDetail.PersonCreate, ProductDetail.PersonUpdate, ProductDetail.Describe, ProductDetail.Status, ProductDetail.Id_Capacity, ProductDetail.Id_Color, ProductDetail.Id_Manufacture, ProductDetail.Id_Category, ProductDetail.Id_Battery, ProductDetail.Id_Chip, ProductDetail.Id_Ram, ProductDetail.Id_Screen, ProductDetail.Id_Size, ProductDetail.Id_Product, ProductDetail.images, ProductDetail.Price, ProductDetail.Quantity from ProductDetail join Product on ProductDetail.Id_Product = Product.Id where ProductDetail.Status = 0 and Product.Price > 20000000", nativeQuery = true)
     Page<ProductDetail> getProductDetailByPriceBigger20000000(Pageable pageable);
 }
